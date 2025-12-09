5758fd58923f69c8000082ad2f0881fdadd7026d
Steven Henry <stevennassef97@gmail.com>
2025-12-09 18:10:55 +0200

    Inline CardSuit and CardValue enums into Card class for simplified structure.

--- Assets/Scripts/Game/Core/Model/Card.cs
+++ Assets/Scripts/Game/Core/Model/Card.cs
@@ -11,4 +11,26 @@ namespace Game.Core.Model
         public CardValue CardValue { get; }
         public CardSuit Suit { get; }
     }
+    
+    public enum CardSuit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+    
+    public enum CardValue
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+    }
 }
