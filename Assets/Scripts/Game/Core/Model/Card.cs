9c7a54633c10240db940c0f0f8ff4bb07aad6923
Steven Henry <stevennassef97@gmail.com>
2025-12-10 17:57:18 +0200

    Add comparer classes to Card for value and suit comparison

--- Assets/Scripts/Game/Core/Model/Card.cs
+++ Assets/Scripts/Game/Core/Model/Card.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Game.Core.Model
 {
     public readonly struct Card
@@ -10,8 +12,32 @@ namespace Game.Core.Model
 
         public CardValue CardValue { get; }
         public CardSuit Suit { get; }
+
+        #region Helpers
+
+        public static ValueOnlyComparer ValueOnlyComparerInstance { get; } = new();
+
+        public static SuitOnlyComparer SuitOnlyComparerInstance { get; } = new();
+
+        public class ValueOnlyComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                return x.CardValue.CompareTo(y.CardValue);
+            }
+        }
+
+        public class SuitOnlyComparer : IComparer<Card>
+        {
+            public int Compare(Card x, Card y)
+            {
+                return x.Suit.CompareTo(y.Suit);
+            }
+        }
+
+        #endregion
     }
-    
+
     public enum CardSuit
     {
         Spades,
@@ -19,7 +45,7 @@ namespace Game.Core.Model
         Diamonds,
         Clubs
     }
-    
+
     public enum CardValue
     {
         Ace = 1,
