e64237a0d87fc0555e3d8d4d197e1e849c6965a2
Steven Henry <stevennassef97@gmail.com>
2025-12-11 16:26:45 +0200

    Add `LocalPlayer` class to represent local player data in the Unity client

---
+++ Assets/Scripts/Game/Client/Runtime/LocalPlayer.cs
@@ -0,0 +1,17 @@
+using Game.Core.Abstractions;
+
+namespace Game.Client.Runtime
+{
+    public class LocalPlayer : IPlayer
+    {
+        public string Id { get; }
+        public string DisplayName { get; }
+
+        public LocalPlayer(string id, string displayName)
+        {
+            Id = id;
+            DisplayName = displayName;
+        }
+    }
+
+}
